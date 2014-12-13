a77e3b0141f6e198991a775decee99db0b16d8ac
Armand Halbert <armand.halbert@gmail.com>
2014-12-13 02:50:24 -0500

    Created moderator page

--- app.js
+++ app.js
@@ -2,13 +2,18 @@ var express = require('express');
 var http = require('http');
 var path = require('path');
 var app = express();
+var roleModule = require('./src/static/roles.js');
 app.set('port', 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/",function(req,res){
-    res.render("home.ejs")
+    res.render("home.ejs");
+});
+
+app.get("/moderator",function(req,res){
+    res.render("moderator.ejs", {roles:roleModule.gameRoles });
 });
 
 var server=http.createServer(app);
---
+++ src/static/roles.js
@@ -0,0 +1,2 @@
+var gameRoles = {werewolf:{ name:"Werewolf" }, villager:{ name:"Villager" }, seer:{ name:"Seer" }, witch:{ name:"Witch" }, bodyguard:{ name:"Bodyguard" }};
+exports.gameRoles = gameRoles;
