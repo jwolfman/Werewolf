736b520c9bce0499b889c6426f306aa12ef0d695
jwolfman <jawjawbinx@yahoo.com>
2014-12-14 21:58:02 -0500

    Created game.js and User.js

---
+++ public/js/game.js
@@ -0,0 +1,25 @@
+function initGame(){
+    var roles=$("#roleDistribution");
+    var roleDistribution=[roles.length];
+    var i=0;
+    for(role in roles){
+        roleDistribution[i]=role;
+        i++;
+    }
+    shuffle(roleDistribution);
+    i=0;
+    for(user in users){
+        if(user.isPlaying){
+            user.role=roleDistribution[i];
+            i++;
+        }
+    }
+}
+function shuffle(roles){
+    for(var c=0;c<roles.length;c++){
+        var temp=roles[c];
+        var num=Math.floor((Math.random()*roles.length)+i);
+        roles[c]=roles[num];
+        roles[num]=temp;
+    }
+}
